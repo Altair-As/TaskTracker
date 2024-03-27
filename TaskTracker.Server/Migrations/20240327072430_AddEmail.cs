6e3df1cd817a97437e053211c3a4b85b120bbf3f
Semen Aleksandrov (EXT) <semen.aleksandrov@rwl.ru>
2024-03-27 10:26:17 +0300

    Add Email field to Database

---
+++ TaskTracker.Server/Migrations/20240327072430_AddEmail.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Email",
+                table: "Employees",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_Email",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+        }
+    }
+}
